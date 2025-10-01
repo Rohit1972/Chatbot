69432f59506e1eb596a5f92a736bf454fd39679f
Rohit Kumar <159619898+Rohit1972@users.noreply.github.com>
2025-10-01 23:57:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Sidebar from "./components/Sidebar/Sidebar"
 import Separation from "./components/separation/Separation"
-import ChatSection from "./components/chatSection/chatSection"
+import ChatSection from "./components/chatSection/ChatSection"
 
 function App() {
  
